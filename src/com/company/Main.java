b7726f8ffb6d66e418720e469905df1913d46ea6
Ekaterina Kovalenko <katrin.kovali2702@gmail.com>
2022-02-23 20:06:11 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < intList.size(); i++) {
+            if (intList.get(i) > 0 && intList.get(i) % 2 == 0) {
+                result.add(intList.get(i));
+            }
+        }
+        Collections.sort(result);
+        for (Integer str : result) System.out.print(" " + str);
+    }
+}
+
+
+class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+    }
+}
+
